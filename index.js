fbbffc46d83a7436f7e9fffdadbed5e9da6209f9
Oleg Poludnenko <ua.oleg@gmail.com>
2022-03-29 13:08:01 +0300

    IDEN-252 Converting golang into javascript

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'snarkjs';
-import 'web3-eth-contract';
+import { ethers } from 'ethers';
 
-async function calculateProof(message, publicSignals, proof) {
+const ETHER_RPC_URL = 'https://ropsten.etherscan.io/address/0xE4F771f86B34BF7B323d9130c385117Ec39377c3';
+
+async function calculateProof(message, contractAddress, publicSignals, proof) {
 
     // Verification of circuits public signals
     const token = extractMetadata(message);
-    let stateInfo = verifyState(ethURL, contractAddress, token);
+    let stateInfo = verifyState(ETHER_RPC_URL, contractAddress, token.ID, token.State);
     console.log(stateInfo);
 
     // groth16 verify
@@ -13,8 +15,8 @@ async function calculateProof(message, publicSignals, proof) {
         return res.json();
     });
     const res  = await snarkjs.groth16.verify(vkey, publicSignals, proof);
-
     console.log(res);
+
 }
 
 function extractMetadata(message) {
@@ -35,15 +37,22 @@ function extractMetadata(message) {
  */
 function verifyState(rpcURL, contractAddress, id, state) {
 
-    // get latest state for id from contract
-    let stateContract = contractCall(contractAddress);
-    if (err) {
-        return err;
-    }
-    if (parseInt(stateContract) == 0) {
-        let err = checkGenesisStateID(id, state);
-        if (err) {
-            return err;
+    const ethersProvider = new ethers.providers.JsonRpcProvider(rpcURL);
+
+    // The ERC-20 Contract ABI, which is a common contract interface
+    // for tokens (this is the Human-Readable ABI format)
+    const daiAbi = [
+        "function getState() view returns (string)",
+        "function getTransitionTimestamp() view returns (int)",
+    ];
+
+    const daiContract = new ethers.Contract(contractAddress, daiAbi, ethersProvider);
+    const stateContract = daiContract.getState(id) + '';
+
+    if (stateContract === '0') {
+        const error = checkGenesisStateID(id, state);
+        if (error) {
+            return {Error: error};
         }
         return {Latest: true, State: state};
     }
@@ -52,30 +61,29 @@ function verifyState(rpcURL, contractAddress, id, state) {
         // The non-empty state is returned, and it’s not equal to the state that the user has provided.
         // Get the time of the latest state and compare it to the transition time of state provided by the user.
         // The verification party can make a decision if it can accept this state based on that time frame
-
-        const timestamp = contractCall(contractAddress)
+        const timestamp = daiContract.getTransitionTimestamp(id);
         if (!timestamp) {
-            return 'no information of transition for non-latest state';
+            return {Error: 'No information of transition for non-latest state'};
         }
 
         return {Latest: false, State: state, TransitionTimestamp: timestamp};
     }
 
     // The non-empty state is returned and equals to the state in provided proof which means that the user state is fresh enough, so we work with the latest user state.
-    return {Latest: true, State: state.String()};
+    return {Latest: true, State: state};
 }
 
-function contractCall(contractAddress) {
+function checkGenesisStateID(id, state) {
 
-    const ethclient = new web3.eth.Contract({
-        provider: rpcURL
-    }, contractAddress);
-    ethclient.send({}).on('receipt', function (outputs) {
-        if (!outputs[0]) {
-            return 'no state output';
-        } else {
-            return outputs[0];
-        }
-    });
+    const stateHash = merkletree.NewHashFromBigInt(state);
+    const IDFromState = core.IdGenesisFromIdenState(stateHash).String();
 
+    const elemBytes = merkletree.NewElemBytesFromBigInt(id);
+    const IDFromParam = core.IDFromBytes(elemBytes[:31]);
+
+    if (IDFromState != IDFromParam) {
+        return "ID from genesis state (" + IDFromState + ") and provided (" + IDFromParam + ") don't match";
+    }
+
+    return null;
 }
