1ed030019929b2c593d45021189a7d764aa5f79d
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-04-20 19:27:31 +0300

    create common structure

---
+++ proofs/zk.js
@@ -0,0 +1,76 @@
+
+
+import { Circuits } from "../circuits/registry.js"
+import 'snarkjs';
+
+export const identifierAttribute = "user_identifier";
+export const challengeAttribute = "challenge";
+export const stateAttribute = "user_state";
+export const zeroKnowledgeProofType = "zeroknowledge";
+
+/**
+ *  ExtractMetadata extracts metadata from proof with zeroknowledge type
+ */
+function ExtractMetadata(proof) {
+    if (proof.type !== zeroKnowledgeProofType) {
+        throw new Error(`Proofs type ${metadata.type} is not zeroknowledge`)
+    }
+    const circuit = Circuits.getCircuit(proof.circuitId)
+    if (!circuit) {
+        throw new Error(`Circuit with id ${proof.circuitId} not found`)
+    }
+    const proofData = parsePublicSignals(proof.pubSignals, circuit.GetPublicSignalsSchema())
+    return proofData;
+}
+
+/**
+ *  VerifyProof verifies proof with zeroknowledge type
+ */
+async function VerifyProof(proof) {
+
+    if (proof.type !== zeroKnowledgeProofType) {
+        throw new Error(`Proofs type ${proof.type} is not zeroknowledge`)
+    }
+    const circuit = Circuits.getCircuit(proof.circuitId)
+    if (!circuit) {
+        throw new Error(`Circuit with id ${proof.circuitId} not found`)
+    }
+
+    return await snarkjs.groth16.verify(circuit.GetVerificationKey(), proof.publicSignals, proof.proofData);
+}
+
+/**
+ * 
+ * @param {[]string} signals 
+ * @param {[]byte} schema 
+ * @returns {ProofMetadata}
+ */
+function parsePublicSignals(signals, schema) {
+    const proofMetadata = new ProofMetadata();
+
+    const metaData = JSON.parse(schema);
+
+    const identifierIndex = metaData[identifierAttribute];
+    if (!identifierIndex) {
+        throw new Error("No user identifier attribute in provided proof");
+    }
+    const stateIndex = metaData[stateAttribute];
+    if (stateIndex) {
+        proofMetadata.authData.userState = signals[stateIndex];
+    }
+    const challengeIndex = metaData[challengeAttribute];
+    if (!challengeIndex) {
+        throw new Error("No user challenge attribute in provided proof")
+    }
+
+    proofMetadata.authData.userIdentifier = convertID(signals[identifierIndex])
+
+    proofMetadata.authData.authenticationChallenge = signals[challengeIndex]
+
+    Object.keys(metaData)
+        .filter(k => ![identifierAttribute, challengeAttribute].includes(k))
+        .forEach(k => proofMetadata.AdditionalData[k] = signals[metaData[k]])
+
+    return proofMetadata;
+}
+
---
+++ state.js
@@ -0,0 +1,92 @@
+
+/**
+ * Verify identity state
+ * @param rpcURL url to connect to the blockchain
+ * @param contractAddress is an address of state contract
+ * @param id is base58 identifier  e.g. id:11A2HgCZ1pUcY8HoNDMjNWEBQXZdUnL3YVnVCUvR5s
+ * @param state is bigint string representation of identity state
+ */
+ async function verifyState(rpcURL, contractAddress, id, state) {
+
+    const stateABI       = [{"inputs":[{"internalType":"address","name":"_verifierContractAddr","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"id","type":"uint256"},{"indexed":false,"internalType":"uint64","name":"blockN","type":"uint64"},{"indexed":false,"internalType":"uint64","name":"timestamp","type":"uint64"},{"indexed":false,"internalType":"uint256","name":"state","type":"uint256"}],"name":"StateUpdated","type":"event"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"getState","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint64","name":"blockN","type":"uint64"}],"name":"getStateDataByBlock","outputs":[{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"getStateDataById","outputs":[{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint64","name":"timestamp","type":"uint64"}],"name":"getStateDataByTime","outputs":[{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"state","type":"uint256"}],"name":"getTransitionInfo","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint64","name":"","type":"uint64"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"newState","type":"uint256"},{"internalType":"uint256","name":"genesisState","type":"uint256"},{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256[2]","name":"a","type":"uint256[2]"},{"internalType":"uint256[2][2]","name":"b","type":"uint256[2][2]"},{"internalType":"uint256[2]","name":"c","type":"uint256[2]"}],"name":"initState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"newState","type":"uint256"},{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256[2]","name":"a","type":"uint256[2]"},{"internalType":"uint256[2][2]","name":"b","type":"uint256[2][2]"},{"internalType":"uint256[2]","name":"c","type":"uint256[2]"}],"name":"setState","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+    const ethersProvider = new ethers.providers.JsonRpcProvider(rpcURL);
+    const contract       = new ethers.Contract(contractAddress, stateABI, ethersProvider);
+    const contractState  = await contract.getState(id);
+
+    if (contractState.toBigInt() === 0n) {
+        const error = checkGenesisStateID(id, state);
+        if (error) {
+            return {Error: error};
+        }
+
+        return {Latest: true, State: state};
+    }
+
+    if (contractState.toBigInt() !== state) {
+
+        // The non-empty state is returned, and it’s not equal to the state that the user has provided.
+        // Get the time of the latest state and compare it to the transition time of state provided by the user.
+        // The verification party can make a decision if it can accept this state based on that time frame
+        // type TransitionInfo struct {
+        //     ReplacedAtTimestamp *big.Int
+        //     CreatedAtTimestamp *big.Int
+        //     ReplacedAtBlock uint64
+        //     CreatedAtBlock uint64
+        //     ReplacedBy *big.Int
+        //     ID *big.Int
+        // }
+        const transitionInfo = await contract.getTransitionInfo(contractState);
+
+        if (transitionInfo[5].toBigInt() === 0n) {
+            return {Error: 'Transition info contains invalid id'};
+        }
+
+        if (transitionInfo[0].toBigInt() === 0n) {
+            return {Error: 'No information of transition for non-latest state'};
+        }
+
+        return {Latest: false, State: state, TransitionTimestamp: transitionInfo[0].toBigInt()};
+    }
+
+    // The non-empty state is returned and equals to the state in provided proof which means that the user state is fresh enough, so we work with the latest user state.
+    return {Latest: true, State: state};
+}
+
+function checkGenesisStateID(id, state) {
+
+    const idBytes = toBufferLE(id, 31);
+
+    // TypeBJP0 specifies the BJ-P0
+    // - first 2 bytes: `00000000 00000000`
+    // - curve of k_op: babyjubjub
+    // - hash function: `Poseidon` with 4+4 elements
+    const typeBJP0 = Buffer.alloc(2);
+    const stateBytes = toBufferLE(state, 32);
+    const idGenesisBytes = stateBytes.slice(-27); // we take last 27 bytes, because of swapped endianness
+    const idFromStateBytes = Buffer.concat([
+        typeBJP0,
+        idGenesisBytes,
+        calculateChecksum(typeBJP0, idGenesisBytes),
+    ]);
+
+    if (!idBytes.equals(idFromStateBytes)) {
+        return "ID from genesis state (" + JSON.stringify(idFromStateBytes.toJSON().data) + ") and provided (" + JSON.stringify(idBytes.toJSON().data) + ") don't match";
+    }
+
+    return null;
+}
+
+function calculateChecksum(type, genesis) {
+    const checksumBytes = Buffer.concat([type, genesis]);
+
+    let sum = 0;
+    for (let val of checksumBytes.values()) {
+        sum += val;
+    }
+
+    const checksum = Buffer.alloc(2);
+    checksum[0] = sum >> 8;
+    checksum[1] = sum & 0xff;
+
+    return checksum;
+}
