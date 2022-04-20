7853e6fa97c51ef8954d482e434bbddc85953da5
Oleg Poludnenko <ua.oleg@gmail.com>
2022-04-20 12:16:48 +0300

    IDEN-252 Converting golang into javascript

--- auth.js
+++ auth.js
@@ -1,9 +1,15 @@
 import 'snarkjs';
 
+const PROTOCOL_NAME                       = 'https://iden3-communication.io'
+const AUTHORIZATION_RESPONSE_MESSAGE_TYPE = PROTOCOL_NAME + '/authorization-response/v1';
+
 async function verifyProofs(message) {
     const verificationKey = await fetch('test/verification_key.json').then(function (res) {
         return res.json();
     });
+    if (message.type !== AUTHORIZATION_RESPONSE_MESSAGE_TYPE) {
+        return `Message type of ${message.type} is not supported`;
+    }
     for (const scope of message.data.scope) {
         switch (scope.type) {
             case 'zeroknowledge':
