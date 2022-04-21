edae667c94f31508e16d879b6c84893dd4023ce6
Oleg Poludnenko <ua.oleg@gmail.com>
2022-04-21 16:37:26 +0300

    IDEN-282 create auth request function

--- auth.js
+++ auth.js
@@ -1,9 +1,10 @@
 
 const PROTOCOL_NAME                       = 'https://iden3-communication.io'
 const AUTHORIZATION_RESPONSE_MESSAGE_TYPE = PROTOCOL_NAME + '/authorization-response/v1';
-const AUTHORIZATION_REQUEST_MESSAGE_TYPE = PROTOCOL_NAME + '/authorization-request/v1';
+const AUTHORIZATION_REQUEST_MESSAGE_TYPE  = PROTOCOL_NAME + '/authorization-request/v1';
 
-
+const AUTH_CIRCUIT_ID           = 'auth';
+const ZERO_KNOWLEDGE_PROOF_TYPE = 'zeroknowledge';
 
 async function verifyProofs(message) {
     if (message.type !== AUTHORIZATION_RESPONSE_MESSAGE_TYPE) {
@@ -14,7 +15,7 @@ async function verifyProofs(message) {
     }
     for (const proof of message.data.scope) {
         switch (proof.type) {
-            case 'zeroknowledge':
+            case ZERO_KNOWLEDGE_PROOF_TYPE:
                 const isValid = zkpVerifyProof(proof);
                 if (!isValid) {
                     return `Proof with type ${proof.type} is not valid`;
@@ -46,4 +47,44 @@ async function extractMetadata(message) {
     return null;
 }
 
+/**
+ * Creates new authorization request message
+ * @param {number} challenge
+ * @param {string} aud
+ * @param {string} callbackURL
+ * @return {Object} AuthorizationMessageRequest
+ */
+function createAuthorizationRequest(challenge, aud, callbackURL) {
+    const message = {
+        type   : AUTHORIZATION_REQUEST_MESSAGE_TYPE,
+        data   : {
+            callbackURL: callbackURL,
+            audience   : aud,
+            scope      : [],
+        },
+        message: null,
+    };
+    messageWithDefaultZKAuth(message, challenge);
 
+    return message;
+}
+
+/**
+ * Adds authentication request to scope
+ * @param {Object} message
+ * @param {number} challenge
+ */
+function messageWithDefaultZKAuth(message, challenge) {
+
+    const rules = {
+        challenge: challenge,
+    };
+
+    const authProofRequest = {
+        type     : ZERO_KNOWLEDGE_PROOF_TYPE,
+        circuitID: AUTH_CIRCUIT_ID,
+        rules    : rules,
+    };
+
+    message.data.scope.push(authProofRequest);
+}
