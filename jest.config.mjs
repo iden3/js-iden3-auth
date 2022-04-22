0fb575ccc0c9f27f1b4c1c2dd219218108e602a0
Kolezhniuk <D.Kolezhiniyk@gmail.com>
2022-04-23 00:19:18 +0300

    add test to gihub actions.

--- jest.config.mjs
+++ jest.config.mjs
@@ -62,7 +62,9 @@ export default {
     // A set of global variables that need to be available in all test environments
     // globals: {},
 
-    // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+    // The maximum amount of workers used to run your tests. Can be specified as % or a number.
+    // E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers:
+    // 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
 
     // An array of directory names to be searched recursively up from the requiring module's location
@@ -80,10 +82,12 @@ export default {
     //   "node"
     // ],
 
-    // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+    // A map from regular expressions to module names or to arrays
+    // of module names that allow to stub out resources with a single module
     // moduleNameMapper: {},
 
-    // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+    // An array of regexp pattern strings, matched against all
+    // module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
 
     // Activates notifications for test results
@@ -174,13 +178,15 @@ export default {
     // A map from regular expressions to paths to transformers
     transform: {},
 
-    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+    // An array of regexp pattern strings that are matched against all
+    // source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
     //   "/node_modules/",
     //   "\\.pnp\\.[^\\/]+$"
     // ],
 
-    // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+    // An array of regexp pattern strings that are matched against
+    // all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
 
     // Indicates whether each individual test should be reported during the run
