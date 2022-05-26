1581370c606b33667b0217fe2150d1fc8aea9662
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-05-26 19:23:25 +0300

    fix loader [x2]

--- test/schema.test.ts
+++ test/schema.test.ts
@@ -1,20 +1,24 @@
 import { HttpSchemaLoader, IpfsSchemaLoader } from '../src/loaders/schema';
+import { Schema } from '../src/protocol/models';
 
 test('schema http loader', async () => {
   const url =
     'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v2.json-ld';
-  const loader = new HttpSchemaLoader(url);
-  const schemaResult = await loader.load();
+  
+  var schema : Schema = {url: url,type:"KYCCountryOfResidenceCredential"};
+  const loader = new HttpSchemaLoader();
+  const schemaResult = await loader.load(schema);
   expect(schemaResult.extension).toEqual('json-ld');
   expect(schemaResult.schema).not.toBeNull();
 });
 
 test('schema ipfs loader', async () => {
   const url =
-    'https://ipfs.infura.io:5001';
-  const cId = 'QmP8NrKqoBKjmKwMsC8pwBCBxXR2PhwSepwXx31gnJxAbP';
-  const loader = new IpfsSchemaLoader(url, cId);
-  const schemaResult = await loader.load();
+  'https://ipfs.infura.io:5001';
+  var schema : Schema = {url: "ipfs://QmP8NrKqoBKjmKwMsC8pwBCBxXR2PhwSepwXx31gnJxAbP",type:"KYCCountryOfResidenceCredential"};
+
+  const loader = new IpfsSchemaLoader(url);
+  const schemaResult = await loader.load(schema);
   expect(schemaResult.extension).toEqual('json-ld');
   expect(schemaResult.schema).not.toBeNull();
 });
