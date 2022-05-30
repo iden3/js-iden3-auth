542ac523b9865c0480d08fd9d7419de6e153fdce
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-05-30 14:42:25 +0300

    change to Object

--- src/loaders/key.ts
+++ src/loaders/key.ts
@@ -1,13 +1,14 @@
 import { promises as fs } from 'fs';
 
 export interface IKeyLoader {
-  load(circuitId: string): Promise<Buffer>;
+  load(circuitId: string): Promise<Object>;
 }
 
 export class FSKeyLoader implements IKeyLoader {
   constructor(public readonly dir: string) {}
-  public async load(curcuitId: string): Promise<Buffer> {
+  public async load(curcuitId: string): Promise<Object> {
     const data = await fs.readFile(`${this.dir}/${curcuitId}.json`, 'utf8');
-    return Buffer.from(data);
+    return  JSON.parse(data);
+
   }
 }
