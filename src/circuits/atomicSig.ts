34138823ff4eec8aec5175bce7cb775b0bea8416
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-06-03 15:19:45 +0300

    fix ===

--- src/circuits/atomicSig.ts
+++ src/circuits/atomicSig.ts
@@ -83,12 +83,12 @@ export class AtomicQuerySigPubSignals implements PubSignalsVerifier {
     return;
   }
   async verifyIdOwnership(sender: string, challenge: bigint): Promise<void> {
-    if (sender != this.userId.string()) {
+    if (sender !== this.userId.string()) {
       throw new Error(
         `sender is not used for proof creation, expected ${sender}, user from public signals: ${this.userId.string()}  `,
       );
     }
-    if (challenge.toString() != this.challenge.toString()) {
+    if (challenge.toString() !== this.challenge.toString()) {
       throw new Error(
         `challenge is not used for proof creation, expected ${challenge}, challenge from public signals: ${this.challenge.toString()}  `,
       );
