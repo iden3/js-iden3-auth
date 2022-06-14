f1fa2d97ac53bf907460c930a40d7fc21f2cd297
Kolezhniuk <D.Kolezhiniyk@gmail.com>
2022-06-14 20:37:08 +0200

    fix alias build

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
         "@app/core/(.*)": "<rootDir>/src/core/$1",
         "@app/protocol/(.*)": "<rootDir>/src/protocol/$1",
         "@app/circuits/(.*)": "<rootDir>src/circuits/$1",
-        "@proofs/(.*)": "<rootDir>src/proofs/$1",
+        "@app/proofs/(.*)": "<rootDir>src/proofs/$1",
         "@app/auth/(.*)": "<rootDir>src/auth/$1",
         "@app/state/(.*)": "<rootDir>src/state/$1",
         "@app/loaders/(.*)": "<rootDir>src/loaders/$1"
