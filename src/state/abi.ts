683938ba08a0e100bc86ca6d8ba2f09ce276c767
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2022-06-21 11:15:23 +0300

    Update contract abi

--- src/state/abi.ts
+++ src/state/abi.ts
@@ -42,73 +42,37 @@ export const stateABI = [
     type: 'event',
   },
   {
-    inputs: [{ internalType: 'uint256', name: 'id', type: 'uint256' }],
-    name: 'getState',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
     inputs: [
-      { internalType: 'uint256', name: 'id', type: 'uint256' },
-      { internalType: 'uint64', name: 'blockN', type: 'uint64' },
-    ],
-    name: 'getStateDataByBlock',
-    outputs: [
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'uint256', name: 'id', type: 'uint256' }],
-    name: 'getStateDataById',
-    outputs: [
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'uint256', name: 'id', type: 'uint256' },
-      { internalType: 'uint64', name: 'timestamp', type: 'uint64' },
-    ],
-    name: 'getStateDataByTime',
-    outputs: [
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'uint256', name: 'state', type: 'uint256' }],
-    name: 'getTransitionInfo',
-    outputs: [
-      { internalType: 'uint256', name: '', type: 'uint256' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint64', name: '', type: 'uint64' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'uint256', name: 'newState', type: 'uint256' },
-      { internalType: 'uint256', name: 'genesisState', type: 'uint256' },
-      { internalType: 'uint256', name: 'id', type: 'uint256' },
-      { internalType: 'uint256[2]', name: 'a', type: 'uint256[2]' },
-      { internalType: 'uint256[2][2]', name: 'b', type: 'uint256[2][2]' },
-      { internalType: 'uint256[2]', name: 'c', type: 'uint256[2]' },
+      {
+        internalType: 'uint256',
+        name: 'newState',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'genesisState',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256[2]',
+        name: 'a',
+        type: 'uint256[2]',
+      },
+      {
+        internalType: 'uint256[2][2]',
+        name: 'b',
+        type: 'uint256[2][2]',
+      },
+      {
+        internalType: 'uint256[2]',
+        name: 'c',
+        type: 'uint256[2]',
+      },
     ],
     name: 'initState',
     outputs: [],
@@ -117,15 +81,151 @@ export const stateABI = [
   },
   {
     inputs: [
-      { internalType: 'uint256', name: 'newState', type: 'uint256' },
-      { internalType: 'uint256', name: 'id', type: 'uint256' },
-      { internalType: 'uint256[2]', name: 'a', type: 'uint256[2]' },
-      { internalType: 'uint256[2][2]', name: 'b', type: 'uint256[2][2]' },
-      { internalType: 'uint256[2]', name: 'c', type: 'uint256[2]' },
+      {
+        internalType: 'uint256',
+        name: 'newState',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256[2]',
+        name: 'a',
+        type: 'uint256[2]',
+      },
+      {
+        internalType: 'uint256[2][2]',
+        name: 'b',
+        type: 'uint256[2][2]',
+      },
+      {
+        internalType: 'uint256[2]',
+        name: 'c',
+        type: 'uint256[2]',
+      },
     ],
     name: 'setState',
     outputs: [],
     stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+    ],
+    name: 'getState',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint64',
+        name: 'blockN',
+        type: 'uint64',
+      },
+    ],
+    name: 'getStateDataByBlock',
+    outputs: [
+      {
+        internalType: 'uint64',
+        name: '',
+        type: 'uint64',
+      },
+      {
+        internalType: 'uint64',
+        name: '',
+        type: 'uint64',
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint64',
+        name: 'timestamp',
+        type: 'uint64',
+      },
+    ],
+    name: 'getStateDataByTime',
+    outputs: [
+      {
+        internalType: 'uint64',
+        name: '',
+        type: 'uint64',
+      },
+      {
+        internalType: 'uint64',
+        name: '',
+        type: 'uint64',
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+    ],
+    name: 'getStateDataById',
+    outputs: [
+      {
+        internalType: 'uint64',
+        name: '',
+        type: 'uint64',
+      },
+      {
+        internalType: 'uint64',
+        name: '',
+        type: 'uint64',
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
     type: 'function',
   },
 ];
