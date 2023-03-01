c118d147fc26526d4a9c2f0517875e9f74b15997
ilya-korotya <officalbatya@gmail.com>
2023-03-01 16:51:22 +0200

    get mt value from path

--- src/circuits/query.ts
+++ src/circuits/query.ts
@@ -3,7 +3,6 @@ import nestedProperty from 'nested-property';
 import { Id, SchemaHash, DID } from '@iden3/js-iden3-core';
 import { Merklizer, Path } from '@iden3/js-jsonld-merklization';
 import keccak256 from 'keccak256';
-import { MtValue } from '@iden3/js-jsonld-merklization/dist/types/lib/mt-value';
 
 const operators: Map<string, number> = new Map([
   ['$noop', 0],
@@ -180,19 +179,8 @@ async function validateDisclosure(
     throw new Error(`can't build path to '${cq.fieldName}' key`);
   }
 
-  let valueByPath: any;
-  try {
-    valueByPath = mz.rawValue(merkalizedPath);
-  } catch (e) {
-    throw new Error(`can't get value by path '${cq.fieldName}'`);
-  }
-
-  const v = mz.mkValue(valueByPath);
-  const mtValue = new MtValue(v);
-
-  if (
-    BigInt(await mtValue.mtEntry()).toString() !== outputs.value[0].toString()
-  ) {
+  const mtValue = await merkalizedPath.mtEntry();
+  if (mtValue !== outputs.value[0]) {
     throw new Error(`value that was used is not equal to requested in query`);
   }
 
