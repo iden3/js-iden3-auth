dd2d14224dbb7e97407de96d49837c64bbbc9807
Kolezhniuk <D.Kolezhiniyk@gmail.com>
2023-04-06 18:35:56 +0200

    Revert key.ts

--- src/loaders/key.ts
+++ src/loaders/key.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import { promises as fs } from 'fs';
 
 export interface IKeyLoader {
   load(circuitId: string): Promise<Uint8Array>;
@@ -7,7 +7,7 @@ export interface IKeyLoader {
 export class FSKeyLoader implements IKeyLoader {
   constructor(public readonly dir: string) {}
   public async load(circuitId: string): Promise<Uint8Array> {
-    const data = fs.readFileSync(`${this.dir}/${circuitId}.json`, 'utf8');
+    const data = await fs.readFile(`${this.dir}/${circuitId}.json`, 'utf8');
     const enc = new TextEncoder();
     return enc.encode(data);
   }
