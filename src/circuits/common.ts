9f56951d0ea247046a3b4ce7022a0ad08f2cda17
vbasiuk <volodymyr.basiuck@gmail.com>
2024-04-09 16:23:25 +0300

    undo

--- src/circuits/common.ts
+++ src/circuits/common.ts
@@ -2,7 +2,7 @@ import { Id, DID } from '@iden3/js-iden3-core';
 import { IStateResolver, ResolvedState, Resolvers } from '@lib/state/resolver';
 import { Hash } from '@iden3/js-merkletree';
 
-export const issuerStateError = new Error(`user state is not valid`);
+export const userStateError = new Error(`user state is not valid`);
 export const gistStateError = new Error(`gist state is not valid`);
 
 export async function checkUserState(
@@ -13,7 +13,7 @@ export async function checkUserState(
   try {
     return await resolver.resolve(userId.bigInt(), userState.bigInt());
   } catch {
-    throw issuerStateError;
+    throw userStateError;
   }
 }
 
